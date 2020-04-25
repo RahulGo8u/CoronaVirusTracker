97a0739a267ae13983ceedd2a02534f62bbbafb0
rrsimt.rahul@gmail.com <HACK@1903>
2020-04-25 04:44:46 +0400

    Adding countries list component

--- corona-virus-tracker/src/app/app.module.ts
+++ corona-virus-tracker/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalCasesListComponent } from './global-cases-list/global-cases-list.component';
 import { Covid19APIService } from '../../services/covid19-api.service';
+import { CountriesListComponent } from './countries-list/countries-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GlobalCasesListComponent
+    GlobalCasesListComponent,
+    CountriesListComponent
   ],
   imports: [
     BrowserModule,
